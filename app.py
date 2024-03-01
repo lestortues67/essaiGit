2a4c166c3a2bf1aac2c48b8717ecf4019ab0c039
lestortues67 <41441746+lestortues67@users.noreply.github.com>
2024-03-01 15:49:07 +0100

    premier commit pour essaiGit à 15h48

---
+++ app.py
@@ -0,0 +1,15 @@
+""" claire à 10h00 commit N°C3
+Date : 27/02/2024 
+Auteur : Christian Doriath 
+Dossier : /Python39/MesDEv/Flask/Flask_codebase2023 
+Fichier : app.py
+Description : app "codebase" une base de données qui contient TOUTE notre base des connaissances
+de code informatique. 
+"""
+ 
+#test_GitAheadN2 29.02.2024
+#bla bla bla 
+#et voilà bibi le koala 
+#aime pas le chocolat 
+
+
